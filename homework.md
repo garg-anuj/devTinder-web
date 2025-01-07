d8d51f30df626fa454bf0a9d6a015e4d3795eef3
garg-anuj <anujgarg2018@gmail.com>
2025-01-08 03:04:33 +0530

    created routes

---
+++ homework.md
@@ -0,0 +1,3 @@
+in unsecure(http) network browser/axios/if-your-are-not-on-same-network does not allow the  cookies to be set in your browser 
+
+so we have to whitelist the domain name
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import NavBar from "./components/NavBar";
-import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Body from "./Body";
+import Login from "./components/Login";
+
 function App() {
   return (
     <>
-      {/* <h1 className="text-blue-600">Vite + React</h1> */}
-      <NavBar />
+      <BrowserRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+        basename="/"
+      >
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
