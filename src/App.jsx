55ed702e29e35b5c2c3ea995f95f4c747fa64803
garg-anuj <anujgarg2018@gmail.com>
2025-01-10 02:48:06 +0530

    created FeedPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import appStore from "./redux/appStore";
 
 import Body from "./Body";
 import Login from "./components/Login";
-import { Provider } from "react-redux";
-import appStore from "./redux/appStore";
+import SignUp from "./components/SignUp";
+import FeedPage from "./page/FeedPage";
 
 function App() {
   return (
@@ -18,7 +20,9 @@ function App() {
         >
           <Routes>
             <Route path="/" element={<Body />}>
+              <Route path="/signup" element={<SignUp />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/feed" element={<FeedPage />} />
             </Route>
           </Routes>
         </BrowserRouter>
