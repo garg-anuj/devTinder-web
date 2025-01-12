718c5829c607be374bc55e7354522cca56893a28
garg-anuj <anujgarg2018@gmail.com>
2025-01-12 20:36:44 +0530

    use Redux for state Feeds management

--- src/page/FeedPage.jsx
+++ src/page/FeedPage.jsx
@@ -1,30 +1,37 @@
-import { useEffect, useState } from "react";
-import { getData } from "../services/api";
-import { API } from "../utils/constant";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+
 import PeopleCard from "../components/PeopleCard";
 
+import { addFeeds } from "../redux/feedSlide";
+import { getData } from "../services/api";
+import { API } from "../utils/constant";
+
 const FeedPage = () => {
-  const [currentFeeds, setCurrentFeeds] = useState([]);
+  const dispatch = useDispatch();
+  const userFeeds = useSelector((state) => state.feedSlice);
 
   useEffect(() => {
     const getFeeds = async () => {
       try {
         const data = await getData(API.USERS_FEED);
         setCurrentFeeds(data?.allUsers);
+
+        dispatch(addFeeds(data?.allUsers));
       } catch (err) {
         console.log(err);
       }
     };
     getFeeds();
-  }, []);
+  }, [dispatch]);
 
-  if (!currentFeeds || currentFeeds.length < 1) {
+  if (!userFeeds || userFeeds.length < 1) {
     return <div>No Data</div>;
   }
 
   return (
     <div className="flex flex-wrap">
-      {currentFeeds.map((feed, idx) => {
+      {userFeeds.map((feed, idx) => {
         return (
           <div key={idx} className="w-60">
             <PeopleCard userFeed={feed} key={idx} />
